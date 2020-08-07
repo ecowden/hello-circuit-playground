306f8922f2e67a705dd629ed4454d9606995c18b
Evan Cowden <evan38109@gmail.com>
2020-08-06 19:43:00 -0500

    makes clickblink work

---
+++ src/clickblink.go
@@ -0,0 +1,30 @@
+package main
+
+// This is the most minimal blinky example and should run almost everywhere.
+
+import (
+	"machine"
+	"time"
+)
+
+const (
+	led     = machine.LED
+	buttonA = machine.BUTTONA
+	buttonB = machine.BUTTONB
+)
+
+func main() {
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	buttonA.Configure(machine.PinConfig{Mode: machine.PinInputPulldown})
+	buttonB.Configure(machine.PinConfig{Mode: machine.PinInputPulldown})
+
+	for {
+		if buttonA.Get() || buttonB.Get() {
+			led.High()
+		} else {
+			led.Low()
+		}
+
+		time.Sleep(time.Millisecond * 10)
+	}
+}
